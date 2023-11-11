7ee152e7d750e93aa73a0048e65832752eebf3dc
= <=>
2023-11-11 12:40:25 +0500

    overlay edited

--- src/app/Components/footer.tsx
+++ src/app/Components/footer.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <div className="p-8 md:text-[18px] text-[15px] bg-gray-500 text-white flex flex-col-reverse md:flex-row justify-between text-center">
-
-      <div className="w-full md:w-5/12">© 2022 Sinp. Made with ❤️ by Codecarnival.</div>
+      <div className="w-full md:w-5/12">
+        © 2022 Sinp. Made with ❤️ by Codecarnival.
+      </div>
       <div className="flex w-full md:w-5/12 justify-center space-x-2 mb-4 md:mb-0">
         {/* Replace 'logo.svg' with your payment logos */}
         <Image src="/amazon.svg" alt="Logo" width={40} height={30} />
@@ -15,5 +16,5 @@ export default function Footer() {
         <Image src="/bitcoin.svg" alt="Logo" width={40} height={30} />
       </div>
     </div>
-  )
+  );
 }
